6c17ec93ad7000a3d5e7bde1ee83f53982d3753b
ryokoike0324 <ryokoike0324@gmail.com>
2024-02-07 10:37:07 +0900

    rubocup,Rspec導入 rubocup初期設定完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/about'
+  get 'static_pages/privacy'
+  get 'static_pages/terms'
+  get 'static_pages/signup_type'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
