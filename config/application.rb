d3a7889ee25a87ac885b7250ef076bb56c2d6a09
ryokoike0324 <ryokoike0324@gmail.com>
2024-02-15 20:30:58 +0900

    Contactモデルのバリデーションに対する基本的なrspecテストの導入 factory_bot導入はこれから

--- config/application.rb
+++ config/application.rb
@@ -32,13 +32,9 @@ module Myapp
     #rspec関連ファイルの生成設定
     config.generators do |g|
       g.test_framework :rspec,
-        fixtures: false,
         view_specs: false,
         helper_specs: false,
-        routing_specs: false,
-        request_specs: false
-      g.factory_bot false
-      end
+        routing_specs: false
     end
   end
 end
