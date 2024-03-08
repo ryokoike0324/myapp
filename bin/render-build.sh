37352f6429cf1252334d18bbe2b46b81a5d5a36e
ryokoike0324 <ryokoike0324@gmail.com>
2024-03-08 10:16:56 +0900

    render-build rails tailwindcss:install削除

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,7 @@ set -o errexit
 bundle install
 
 # Install tailwindcss-rails gem and generate tailwind.config.js file
-./bin/rails tailwindcss:install
+# ./bin/rails tailwindcss:install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
