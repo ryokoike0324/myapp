1885778669d627f72a6f80d14fc7ab20fcfc31ab
ryokoike0324 <ryokoike0324@gmail.com>
2024-03-08 11:52:34 +0900

    render-build.sh修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,5 +8,6 @@ bundle install
 # ./bin/rails tailwindcss:install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:reset
+bundle exec rails db:drop
+bundle exec rails db:create
 bundle exec rails db:migrate
