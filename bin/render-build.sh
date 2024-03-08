92baa3bbc81f5989873a6bf30620640a3f13426c
ryokoike0324 <ryokoike0324@gmail.com>
2024-03-08 09:09:24 +0900

    render-build.sh修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,6 @@ set -o errexit
 bundle install
 
 # Install tailwindcss-rails gem and generate tailwind.config.js file
-./bin/bundle add tailwindcss-rails
 ./bin/rails tailwindcss:install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
